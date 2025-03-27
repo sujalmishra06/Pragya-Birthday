9ddc59a44a09a9ea5b621e2bccdda9a702d68ea7
Sujal Mishra <114754416+sujalmishra06@users.noreply.github.com>
2025-03-27 17:06:36 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,18 @@
+document
+  .getElementById("signupform")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const validUsername = "admin";
+    const validPassword = "cutestgirl";
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("error-message");
+
+    if (username === validUsername && password === validPassword) {
+      window.location.href = "index.html";
+    } else {
+      errorMessage.textContent = "Invalid username or password!";
+    }
+  });
